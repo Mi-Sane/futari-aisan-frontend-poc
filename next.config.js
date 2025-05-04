84f504b39aee35e9e119fb18d10f3b2c003a858d
Miyu Sanematsu <miyu.saneco@gmail.com>
2025-05-04 16:13:52 +0900

    一時的ログ確認用

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+console.log("NEXT_PUBLIC_API_BASE_URL =", process.env.NEXT_PUBLIC_API_BASE_URL);
+
 const nextConfig = {
   output: 'standalone',
   eslint: {
