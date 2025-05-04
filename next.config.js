7d7e2a77421f6aa9c07a0706c0f829997e8017d8
Miyu Sanematsu <miyu.saneco@gmail.com>
2025-05-04 16:29:41 +0900

    configにenv追記

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-console.log("NEXT_PUBLIC_API_BASE_URL =", process.env.NEXT_PUBLIC_API_BASE_URL);
-
 const nextConfig = {
   output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
 };
 
